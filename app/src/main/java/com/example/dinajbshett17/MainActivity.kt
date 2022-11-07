958fa920f1d07c2fcc35160b226348575181b4df
jobsheet18 <kothim@gmail.com>
2022-11-07 07:16:37 +0700

    first commit

---
+++ app/src/main/java/com/example/dinajbshett17/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.dinajbshett17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tab = findViewById<TabLayout>(R.id.tabLayout)
+        var fragment : Fragment = tpfl()
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+
+        tab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab!!.position) {
+                    0 -> fragment = tpfl()
+                    1 -> fragment = to()
+                    2 -> fragment = pplg()
+                    3 -> fragment = kuliner()
+                    4 -> fragment = busana()
+                }
+
+                val fT = supportFragmentManager.beginTransaction()
+                fT.replace(R.id.frameLayout, fragment)
+                fT.commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+    }
+}
